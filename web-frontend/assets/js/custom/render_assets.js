dd53d5313323aeeae78f743f7309716ce1a31907
Ruelito-Ytac <ruelito.ytac@village88.com>
2023-01-24 17:04:20 +0800

    Update changes

--- web-frontend/assets/js/custom/render_assets.js
+++ web-frontend/assets/js/custom/render_assets.js
@@ -1,19 +1,26 @@
 let assets_data = [
-    /* JS Vendor */
-    {url: "/web-frontend/assets/js/vendor/bootstrap/bootstrap.bundle.min.js", type: "script"},
-
-    /* CUSTOM JS */
-    {url: current_js, type: "script"},
-
     /* CSS Vendor */
     {url: "/web-frontend/assets/css/vendor/bootstrap/bootstrap.min.css", type: "style"},
 
     /* CUSTOM CSS */
-    {url: current_css, type: "style"}
+    {url: current_css, type: "style"},
+
+    /* JS Vendor */
+    {url: "/web-frontend/assets/js/vendor/bootstrap/bootstrap.bundle.min.js", type: "script"},
+
+    /* CUSTOM JS */
+    {url: current_js, type: "script"}
 ];
+
+/* Call back to check if the assets is loaded. */
 let callback = () => {
     console.log("Loaded!");
 };
+
+/* load the vendor for asset loader. */
 let loader = new asyncAssetsLoader();
 
-loader.load(assets_data, callback);
+document.addEventListener("DOMContentLoaded", () => {
+    document.querySelector("body").classList.remove("hidden");
+    loader.load(assets_data, callback);
+});
