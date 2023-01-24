26b3ee5bfb96dc6aa047f46b76a00bf5ca57b492
Ruelito-Ytac <ruelito.ytac@village88.com>
2023-01-24 16:49:21 +0800

    update name changes

--- web-frontend/assets/js/custom/render_assets_css.js
+++
@@ -1,7 +0,0 @@
-let selected_assets = [
-    "/web-frontend/assets/css/vendor/bootstrap/bootstrap.min.css"
-];
-
-for(let index in selected_assets){
-    document.getElementsByTagName("head")[ITEMS.first].innerHTML += `<link rel="stylesheet" href="${ selected_assets[index] }"></link>`;
-}
