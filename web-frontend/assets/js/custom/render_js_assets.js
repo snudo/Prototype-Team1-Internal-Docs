a0c66790a9dc22b0fd9bd504806937314adc6bf4
Ruelito-Ytac <ruelito.ytac@village88.com>
2023-01-24 16:46:40 +0800

    Update changes

--- web-frontend/assets/js/custom/render_js_assets.js
+++ web-frontend/assets/js/custom/render_js_assets.js
@@ -1,7 +1,14 @@
 let assets_data = [
+    /* JS Vendor */
     {url: "/web-frontend/assets/js/vendor/bootstrap/bootstrap.bundle.min.js", type: "script"},
+
+    /* CUSTOM JS */
     {url: current_js, type: "script"},
+
+    /* CSS Vendor */
     {url: "/web-frontend/assets/css/vendor/bootstrap/bootstrap.min.css", type: "style"},
+
+    /* CUSTOM CSS */
     {url: current_css, type: "style"}
 ];
 let callback = () => {
