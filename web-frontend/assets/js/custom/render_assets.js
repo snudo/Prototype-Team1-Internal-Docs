8ebdc39bc93e01838f268fdfd43024e832ae4284
Ruelito-Ytac <ruelito.ytac@village88.com>
2023-01-25 08:45:34 +0800

    Update changes

--- web-frontend/assets/js/custom/render_assets.js
+++ web-frontend/assets/js/custom/render_assets.js
@@ -1,9 +1,7 @@
 let assets_data = [
     /* CSS Vendor */
     {url: "/web-frontend/assets/css/vendor/bootstrap/bootstrap.min.css", type: "style"},
-
-    /* CUSTOM CSS */
-    {url: current_css, type: "style"},
+    {url: "/web-frontend/assets/css/custom/global.css", type: "style"},
 
     /* JS Vendor */
     {url: "/web-frontend/assets/js/vendor/bootstrap/bootstrap.bundle.min.js", type: "script"},
@@ -20,7 +18,4 @@ let callback = () => {
 /* load the vendor for asset loader. */
 let loader = new asyncAssetsLoader();
 
-document.addEventListener("DOMContentLoaded", () => {
-    document.querySelector("body").classList.remove("hidden");
-    loader.load(assets_data, callback);
-});
+loader.load(assets_data, callback);
