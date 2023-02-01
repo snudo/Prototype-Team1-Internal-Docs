e4a0b5c1ca95bedd27b2811e15413ec463180568
Jessie de Leon <jdeleon@village88.com>
2023-02-01 14:06:11 +0800

    - (Documentation) Remove red border on focus when previous input was invalid - (Documentation) Setup a way to easily view sample documentations for small or big data - (Documentation) Add documentation plus sign should also trigger adding of new documentation - Update Index page URLs

--- web-frontend/assets/js/custom/admin/documentation.js
+++ web-frontend/assets/js/custom/admin/documentation.js
@@ -4,33 +4,199 @@ var confirm_private_modal = new bootstrap.Modal(confirm_modal, {});
 var document_id = "";
 var selected_document = "";
 
+/* Get params from URL of current Page */
+let url_obj = new URL((window.location.href).toLowerCase());
+let doc_count = url_obj.searchParams.get("size") || 5;
+
 let documents_array = [
     {
         id: 105,
-        title: "Hello Ruel",
+        title: "Ruel's Document",
         viewers: 10,
         editors: 2,
         is_private: false,
         is_starred: true,
-        description: "ASUS TUF Gaming F15 is a powerful Windows 10 gaming laptop that combines gaming performance with up to a narrow bezel IPS-type panel and an extended lifespan"
+        description: "My Experience in Village88 they taught me how to design a web UI/UX in 3 months."
     },
     {
         id: 106,
-        title: "Hello Fitz",
+        title: "Fitz's Document",
         viewers: 5,
         editors: 3,
         is_private: true,
-        is_starred: false,
-        description: "ASUS TUF Gaming F15 is a powerful Windows 10 gaming laptop that combines gaming performance with up to a narrow bezel IPS-type panel and an extended lifespan"
+        is_starred: true,
+        description: "Village88 gave me an opportunity to start a new career as a Software Engineer."
     },
     {
         id: 107,
-        title: "Hello Stan",
+        title: "Stan's Document",
         viewers: 1,
         editors: 1,
         is_private: true,
+        is_starred: true,
+        description: "The way to Village88 was not easy. Every task during my training was always under time pressure."
+    },
+    {
+        id: 108,
+        title: "Jessie's Document",
+        viewers: 7,
+        editors: 1,
+        is_private: false,
+        is_starred: true,
+        description: "I had previous jobs before Village88, but then I felt like I wasn't learning anything from it anymore."
+    },
+    {
+        id: 109,
+        title: "Bylaws",
+        viewers: 17,
+        editors: 12,
+        is_private: true,
         is_starred: false,
-        description: "ASUS TUF Gaming F15 is a powerful Windows 10 gaming laptop that combines gaming performance with up to a narrow bezel IPS-type panel and an extended lifespan"
+        description: "Bylaws are the laws that the company has established to keep the organisation in control and in regulation. A company’s bylaws are the direct guidance given by the authority of a company."
+    },
+    {
+        id: 110,
+        title: "Non-Disclosure Agreement",
+        viewers: 2,
+        editors: 1,
+        is_private: false,
+        is_starred: false,
+        description: "This information is very critical to the company and if leaked could cost a fortune. Companies, therefore, ask employees to handle confidential company information to sign a non-disclosure agreement."
+    },
+    {
+        id: 111,
+        title: "Operating Agreement",
+        viewers: 107,
+        editors: 25,
+        is_private: true,
+        is_starred: false,
+        description: "An Operating Agreement LLC is a key legal document that is necessary for every LLC with more than 2 or 3 members. Though it is not a legal document, it helps define the process of decision making among key operating authorities."
+    },
+    {
+        id: 112,
+        title: "Minutes of Meeting",
+        viewers: 17,
+        editors: 12,
+        is_private: true,
+        is_starred: false,
+        description: "Minutes of the meeting captures the happenings of a meeting in detail and used as a referring document for the consecutive meetings."
+    },
+    {
+        id: 113,
+        title: "Employment Agreement",
+        viewers: 170,
+        editors: 2,
+        is_private: false,
+        is_starred: false,
+        description: "This agreement sets the ground rules of the relationship between the organisation and the employee. The culture and the vision of the company can be seen in action in the employee agreement."
+    },
+    {
+        id: 114,
+        title: "Business plans",
+        viewers: 10,
+        editors: 1,
+        is_private: true,
+        is_starred: false,
+        description: "A business plan, as the name suggests, is the plan for your business on paper. Before creating a business plan do thorough research, analyze the market, your competitors, understand how you will target the customers in your market. It offers clarity on what your business is, its opportunities and the roadmap that will lead you there."
+    },
+    {
+        id: 115,
+        title: "Business Reports",
+        viewers: 17,
+        editors: 1,
+        is_private: false,
+        is_starred: false,
+        description: "Business reports are facts and figures about certain departments. It includes aspects of your business that exist as company data, which, after analysis can derive outcomes and projections."
+    },
+    {
+        id: 116,
+        title: "Financial Agreement",
+        viewers: 20,
+        editors: 12,
+        is_private: true,
+        is_starred: false,
+        description: "As a business owner, you must have a strong grasp of your company’s finances. It is important to know whether you are within budget, making profits or spiralling losses."
+    },
+    {
+        id: 117,
+        title: "Transactional Documents",
+        viewers: 17,
+        editors: 12,
+        is_private: true,
+        is_starred: false,
+        description: "One of the key factors in keeping your company’s financial statements organised is through the maintenance of transactional documents."
+    },
+    {
+        id: 118,
+        title: "Business Insurance",
+        viewers: 107,
+        editors: 3,
+        is_private: false,
+        is_starred: false,
+        description: "Businesses are prone to risks all the time and having business insurance gives you the safety net for the same."
+    },
+    {
+        id: 119,
+        title: "Partnership Agreement",
+        viewers: 18,
+        editors: 1,
+        is_private: true,
+        is_starred: false,
+        description: "The agreement essentially keeps all the partners in tandem to the agreed terms of working without any deviation and disagreements."
+    },
+    {
+        id: 120,
+        title: "Co-founder Agreement",
+        viewers: 5,
+        editors: 2,
+        is_private: false,
+        is_starred: false,
+        description: "The Co-founder agreement is prepared across more than one founder and highlights the rules and obligations and share of each of the partners."
+    },
+    {
+        id: 121,
+        title: "Business Pitch Deck",
+        viewers: 50,
+        editors: 2,
+        is_private: false,
+        is_starred: false,
+        description: "New companies should create a powerful and insightful pitch deck that will essentially capture the essence of the company."
+    },
+    {
+        id: 122,
+        title: "Investor Agreement",
+        viewers: 5,
+        editors: 2,
+        is_private: false,
+        is_starred: false,
+        description: "Investments are critical to companies along with their different growth phases. An investor agreement is prepared for documenting subscription of shares against the investments done."
+    },
+    {
+        id: 123,
+        title: "Board Resolutions",
+        viewers: 25,
+        editors: 7,
+        is_private: false,
+        is_starred: false,
+        description: "Board Resolutions are decisions taken in a board meeting and form a part of the documentation for further reference."
+    },
+    {
+        id: 124,
+        title: "ESOPs Agreement",
+        viewers: 5,
+        editors: 2,
+        is_private: false,
+        is_starred: false,
+        description: "Employee stock option plans, also known as ESOPsare the stocks offered by a company to its employees at a discounted price."
+    },
+    {
+        id: 125,
+        title: "Accounting Agreement",
+        viewers: 3,
+        editors: 1,
+        is_private: true,
+        is_starred: false,
+        description: "Accounting agreement is a document of mutual understanding between the bookkeeper and the company, discussing services and arrangements."
     }
 ];
 
@@ -44,31 +210,34 @@ const renderDocuments = () => {
         documents_array = [...new Map(documents_array.map(item => [item["id"], item])).values()];
 
         for(let index in documents_array){
-            let document_item = documents_array[index];
-            let cloned_document = document.getElementById("clone").cloneNode(true);
+            /* Only display specified size from the URL */
+            if(index < parseInt(doc_count)){
+                let document_item = documents_array[index];
+                let cloned_document = document.getElementById("clone").cloneNode(true);
 
-            cloned_document.id = document_item.id;
-            cloned_document.querySelectorAll(".document_title")[ITEMS.first].textContent = document_item.title;
-            cloned_document.querySelectorAll(".viewers_count")[ITEMS.first].textContent = document_item.viewers;
-            cloned_document.querySelectorAll(".editors_count")[ITEMS.first].textContent = document_item.editors;
-            cloned_document.querySelectorAll(".document_information p")[ITEMS.first].textContent = document_item.description;
+                cloned_document.id = document_item.id;
+                cloned_document.querySelectorAll(".document_title")[ITEMS.first].textContent = document_item.title;
+                cloned_document.querySelectorAll(".viewers_count")[ITEMS.first].textContent = document_item.viewers;
+                cloned_document.querySelectorAll(".editors_count")[ITEMS.first].textContent = document_item.editors;
+                cloned_document.querySelectorAll(".document_information p")[ITEMS.first].textContent = document_item.description;
 
-            (document_item.is_private) ? cloned_document.classList.add("is_private") : cloned_document.classList.remove("is_private");
-            (document_item.is_starred) ? cloned_document.querySelector("input[type=checkbox]").checked = true : cloned_document.querySelector("input[type=checkbox]").checked = false;
+                (document_item.is_private) ? cloned_document.classList.add("is_private") : cloned_document.classList.remove("is_private");
+                (document_item.is_starred) ? cloned_document.querySelector("input[type=checkbox]").checked = true : cloned_document.querySelector("input[type=checkbox]").checked = false;
 
-            (document_item.is_starred) ? cloned_document.setAttribute("data-starred", "all_starred") : cloned_document.removeAttribute("data-star");
-            (document_item.is_private) ? cloned_document.setAttribute("data-private", "all_private") : cloned_document.removeAttribute("data-private");
+                (document_item.is_starred) ? cloned_document.setAttribute("data-starred", "all_starred") : cloned_document.removeAttribute("data-star");
+                (document_item.is_private) ? cloned_document.setAttribute("data-private", "all_private") : cloned_document.removeAttribute("data-private");
 
-            document.getElementById("document_list_container").appendChild(cloned_document);
+                document.getElementById("document_list_container").appendChild(cloned_document);
 
-            new bootstrap.Popover(cloned_document.querySelector(".documents_menu"), {
-                animation: true,
-                container: "body",
-                content: popover_content,
-                html: true,
-                trigger: "focus",
-                delay: {"hide": ANIMATION_TIME.hide}
-            });
+                new bootstrap.Popover(cloned_document.querySelector(".documents_menu"), {
+                    animation: true,
+                    container: "body",
+                    content: popover_content,
+                    html: true,
+                    trigger: "focus",
+                    delay: {"hide": ANIMATION_TIME.hide}
+                });
+            }
         }
     }
     else{
@@ -82,6 +251,10 @@ renderDocuments();
 
 const getDocumentValue = (event) => {
     event.preventDefault();
+
+    /* Remove red border */
+    event.target.closest("label").classList.remove("input_error");
+    
     if(event.keyCode === 13) {
         if(event.target.value.length){
             let timestamp = new Date().getUTCMilliseconds();
@@ -197,4 +370,9 @@ document.addEventListener("click", DuplicateDocument);
 document.addEventListener("click", FilterDocuments);
 document.getElementById("add_documentation_input").addEventListener("keyup", getDocumentValue);
 
-$(function() {$("#document_list_container").sortable();});
+$(function(){
+    $("#document_list_container").sortable();
+
+    /* Onload focus Add Documentation input box */
+    document.getElementById("add_documentation_input").focus();
+});
